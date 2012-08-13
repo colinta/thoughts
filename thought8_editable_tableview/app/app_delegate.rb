7f71e047d357df3916c0dcf2be58812919e5c50c
Colin Thomas-Arnold <colinta@gmail.com>
2012-08-13 10:07:43 -0600

    like a boss

--- thought8_editable_tableview/app/app_delegate.rb
+++ thought8_editable_tableview/app/app_delegate.rb
@@ -4,6 +4,18 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
+    UINavigationBar.appearance.setBackgroundImage(UIImage.imageNamed("table-bg"), forBarMetrics: UIBarMetricsDefault)
+    UINavigationBar.appearance.tintColor = UIColor.colorWithRed(0.294117647059, green:0.0, blue:0.509803921569, alpha: 0)
+
+    back_normal = UIImage.imageNamed("btn-back").resizableImageWithCapInsets([0, 14, 0, 6])
+    back_highlighted = UIImage.imageNamed("btn-back-pressed").resizableImageWithCapInsets([0, 14, 0, 6])
+    bar_button_normal = UIImage.imageNamed("btn-button").resizableImageWithCapInsets([0, 5.5, 0, 5.5])
+    bar_button_highlighted = UIImage.imageNamed("btn-button-pressed").resizableImageWithCapInsets([0, 5.5, 0, 5.5])
+    UIBarButtonItem.appearance.setBackButtonBackgroundImage(back_normal, forState: UIControlStateNormal, barMetrics: UIBarMetricsDefault)
+    UIBarButtonItem.appearance.setBackButtonBackgroundImage(back_highlighted, forState: UIControlStateHighlighted, barMetrics: UIBarMetricsDefault)
+    UIBarButtonItem.appearance.setBackgroundImage(bar_button_normal, forState: UIControlStateNormal, barMetrics: UIBarMetricsDefault)
+    UIBarButtonItem.appearance.setBackgroundImage(bar_button_highlighted, forState: UIControlStateHighlighted, barMetrics: UIBarMetricsDefault)
+
     mine = MyApplicationController.alloc.init
     nav_ctlr = UINavigationController.alloc.initWithRootViewController(mine)
 
